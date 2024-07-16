e3aeeea054d08e40d399c0d3011dffb6da3e2d89
Matias <mati.dehoyosmdp@gmail.com>
2024-07-16 11:56:28 -0300

    }

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Asegúrate de que este directorio coincida con el especificado en vercel.json
+  },
+  server: {
+    port: 5173,
+  },
+});
