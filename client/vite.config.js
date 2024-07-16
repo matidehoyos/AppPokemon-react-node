d8cad93a4931fbb6f9c3704e007b3ee1805b1cf6
Matias <mati.dehoyosmdp@gmail.com>
2024-07-16 11:33:48 -0300

    }

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
-// vite.config.js
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    outDir: 'dist', 
+  },
   server: {
     port: 5174,
   },
-  build: {
-    outDir: 'dist',
-  },
 });
+
+
