b7b32ed610590e3f8289a2b509ad3af4f8d80c6f
Matias <mati.dehoyosmdp@gmail.com>
2024-12-16 11:07:52 +0000

    end

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+require('dotenv').config();
 require('./db.js');
 
 const server = express();
@@ -14,17 +14,31 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
-  res.header('Access-Control-Allow-Credentials', 'true');
+
+const allowedOrigins = [
+  process.env.URL_UNO, process.env.URL_DOS, process.env.URL_TRES
+];
+
+server.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204); 
+  }
+
   next();
 });
 
 server.use('/pokemons', routes);
 
-server.use((err, res) => { 
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
